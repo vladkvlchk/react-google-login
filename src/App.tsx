4101cc103b4078bdf6382e0631f44f8170cabb9c
Vladyslav <prostoandrei9@gmail.com>
2023-06-06 11:37:00 +0300

    it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import jwt_decode from "jwt-decode";
+
+interface GoogleDataType {
+  aud: string;
+  azp: string;
+  email: string;
+  email_verified: boolean;
+  exp: number;
+  family_name: string;
+  given_name: string;
+  iat: number;
+  iss: string;
+  jti: string;
+  name: string;
+  nbf: number;
+  picture: string;
+  sub: string;
+}
 
 function App() {
+  const [googleData, setGoogleData] = React.useState<
+    GoogleDataType | undefined
+  >();
+
+  const handleCallbackResponse = (response: any) => {
+    if (response.credential) {
+      setGoogleData(jwt_decode(response.credential));
+    } else {
+      console.error(response);
+    }
+  };
+
+  const onClickLogOut = () => {
+    setGoogleData(undefined);
+  };
+  React.useEffect(() => {
+    /* global google */
+    // @ts-ignore
+    google.accounts.id.initialize({
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      callback: handleCallbackResponse,
+    });
+    // @ts-ignore
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, [googleData]);
+
+  if (googleData) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <picture>
+            <img src={googleData.picture} alt="logo" />
+          </picture>
+          <h1>{`Hello, ${googleData.name}!`}</h1>
+          <div>
+            <button onClick={onClickLogOut}>Log out</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Google Login App</h1>
+        <div>
+          <div id="signInDiv"></div>
+        </div>
       </header>
     </div>
   );
